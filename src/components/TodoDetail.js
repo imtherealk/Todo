c703bc2d1186a65011a5d0841a5737c8e747ac18
김나영 <imtherealk@gmail.com>
2016-02-22 18:32:20 +0900

    Create TodoDetail component

--- src/components/TodoDetail.js
+++ src/components/TodoDetail.js
@@ -1,30 +1,59 @@
 import React from 'react';
-import $ from 'jquery'
-import RefreshIndicator from 'material-ui/lib/refresh-indicator';
+import {
+  List,
+  ListItem,
+  TextField,
+  Checkbox
+} from 'material-ui';
 
 import TodoList from './TodoList';
-import {loadTodo} from '../actions/TodoActionCreators';
+import {loadDetail, checkTodo} from '../actions/TodoActionCreators';
+import {emulateEvent} from 'util';
 
 export default class extends React.Component {
-  constructor(props) {
-    super(props);
-    //this.state = {
-    //  todos: this.props
-    //}
-  };
-
   componentDidMount() {
-    loadTodo();
+    emulateEvent(loadDetail(this.props.id));
   }
 
   componentWillUnmount() {
   }
 
+  onCheck(event, id) {
+    checkTodo(id);
+  }
+
   render() {
-    let {todo} = this.props;
+    let {id, todo} = this.props;
     return (
-      <div>
-      </div>
+      <List>
+        <ListItem>
+          <TextField
+            key="content"
+            disabled={true}
+            value={todo.content}
+            floatingLabelText="할 일"/>
+        </ListItem>
+        <ListItem>
+          <TextField
+            key="description"
+            disabled={true}
+            value={todo.description}
+            multiLine={true}
+            floatingLabelText="자세히"/>
+        </ListItem>
+        <ListItem>
+          <TextField
+            key="created_at"
+            value={todo.created_at}
+            multiLine={true}
+            disabled={true}
+            floatingLabelText="작성시간"/>
+        </ListItem>
+        <ListItem primaryText="했나?" rightIcon={
+          <Checkbox onCheck={(e) => this.onCheck(e, todo.id)}
+                    defaultChecked={todo.checked}/>
+        }/>
+      </List>
     )
   }
 }
