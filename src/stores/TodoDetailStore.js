8e3d32ce799a89295883ecf6d0b57bd3ed826726
김나영 <imtherealk@gmail.com>
2016-02-22 18:29:10 +0900

    Implement TodoDetailStore

--- src/stores/TodoDetailStore.js
+++ src/stores/TodoDetailStore.js
@@ -1,3 +1,26 @@
-/**
- * Created by Nayeong on 2016. 2. 22..
- */
+import _ from 'lodash';
+import { ReduceStore } from 'flux/utils';
+import TodoDispatcher from '../dispatcher/TodoDispatcher';
+import {loadTodo} from '../actions/TodoActionCreators'
+import {STATUS} from '../actions/TodoActionCreators';
+
+class TodoDetailStore extends ReduceStore {
+  getInitialState() {
+    return {
+      item: []
+    }
+  }
+
+  reduce(state, action) {
+    switch(action.type) {
+      case 'todo/detail':
+        return _.assign({}, state, {
+          item: action.payload
+        });
+      default:
+        return state;
+    }
+  }
+}
+
+export default new TodoDetailStore(TodoDispatcher)
