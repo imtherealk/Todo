0f4ca16d02a5db19ee58ff07c65b8a8b69658e4e
김나영 <imtherealk@gmail.com>
2016-02-22 18:33:14 +0900

    Route detail container

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -3,10 +3,12 @@ import { Container } from 'flux/utils';
 import { Router, Route } from 'react-router';
 
 import TodoStore from '../stores/TodoStore';
+import TodoDetailStore from '../stores/TodoDetailStore';
 import Header from './Header';
 import Footer from './Footer';
 import MainSection from './MainSection';
-import {loadTodo} from '../actions/TodoActionCreators'
+import TodoDetail from './TodoDetail';
+import {loadTodo} from '../actions/TodoActionCreators';
 
 class Main extends React.Component {
   static getStores() {
@@ -54,11 +56,26 @@ class About extends React.Component {
 }
 
 class Detail extends React.Component {
+  static getStores() {
+    return [TodoDetailStore];
+  }
+
+  static calculateState(prevState) {
+    let state = TodoDetailStore.getState();
+    console.log(state);
+    return {
+      todo: state.item,
+      status: state.status
+    };
+  }
   render() {
     let {params} = this.props;
-    return <TodoDetail/>;
+    return <TodoDetail id={params.id} {...this.state}/>;
   }
 }
+
+export const DetailContainer = Container.create(Detail);
+
 
 /**
  * "/" - Main
@@ -70,7 +87,7 @@ export default class extends React.Component {
       <Router>
         <Route path="/" component={AppLayout}>
           <Route path="about" component={About}/>
-          <Route path="todos/:id" component={Detail}/>
+          <Route path="todos/:id" component={DetailContainer}/>
         </Route>
       </Router>
     )
