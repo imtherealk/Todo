a917436ecbb51799eb12ebf8e6a2becd380508e5
김나영 <imtherealk@gmail.com>
2016-03-06 18:07:11 +0900

    코드 정리

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,42 @@
 var path = require('path');
 
 module.exports = {
-    entry: "./src/app.js",
-    output: {
-        path: __dirname + '/build',
-        filename: "bundle.js"
-    },
-    resolve: {
-        root: path.resolve('./src')
-    },
-    devtool: 'source-map',
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015', 'react']
-                }
-            },
-            {test: /\.css$/, loader: "style!css"},
-            {test: /\.less$/, loader: "style!css!less"},
-            {test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery'},
+  entry: "./src/app.js",
+  output: {
+    path: __dirname + '/build',
+    filename: "bundle.js"
+  },
+  resolve: {
+    root: path.resolve('./src')
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {test: /\.css$/, loader: "style!css"},
+      {test: /\.less$/, loader: "style!css!less"},
+      {test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery'},
 
-            {
-                test: /\.woff2?(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url?limit=10000&mimetype=application/font-woff"
-            },
-            {
-                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url?limit=10000&mimetype=application/octet-stream"
-            },
-            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
-            {
-                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url?limit=10000&mimetype=image/svg+xml"
-            }
-        ]
-    }
+      {
+        test: /\.woff2?(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/octet-stream"
+      },
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=image/svg+xml"
+      }
+    ]
+  }
 };
